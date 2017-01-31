ed17545ec33fe4db6a0c03ac4553200694a491bd
SebQuenet <s.quenet@orange.fr>
2017-01-31 21:44:22 +0100

    DZS-01 - adding koa requirements

---
+++ server/server.js
@@ -0,0 +1,21 @@
+/**
+ * Daozhishi entry point
+ */
+
+'use strict';
+
+// HTTP Framework & Routers 
+const koa = require('koa');
+const kRouter = require('koa-router');
+const kBodyParser = require('koa-bodyparser');
+const kCompress = require('koa-compress');
+const kError = require('koa-error');
+const kSocket = require('koa-socket');
+const kcors = require('kcors');
+
+
+const dzsInstance = (params) => {
+};
+
+
+module.exports = dzsInstance;
